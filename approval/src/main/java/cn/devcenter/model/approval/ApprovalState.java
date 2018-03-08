ca9b001d693f153acb8715616543616991e27479
gaosong <gaosong@housecenter.cn>
2018-03-08 16:24:49 +0800

    init

--- approval/src/main/java/cn/devcenter/model/approval/ApprovalState.java
+++ approval/src/main/java/cn/devcenter/model/approval/ApprovalState.java
@@ -1,7 +1,17 @@
 package cn.devcenter.model.approval;
 
 public enum ApprovalState {
-
-    INIT, APPROVED, REJECTED;
+    /**
+     * 初始化状态，没有审批结果的状态
+     */
+    INIT,
+    /**
+     * 审批同意
+     */
+    APPROVED,
+    /**
+     * 审批拒绝
+     */
+    REJECTED;
 
 }
