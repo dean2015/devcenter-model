7400df57488d9a72809fda6e040a626aa12b26c5
gaosong <gaosong@housecenter.cn>
2018-03-08 16:54:03 +0800

    init

---
+++ execution-result/src/main/java/cn/devcenter/model/result/ExecutionResult.java
@@ -0,0 +1,61 @@
+package cn.devcenter.model.result;
+
+import lombok.Data;
+
+@Data
+public class ExecutionResult<E> {
+
+    public static final int SUCCESS = 0;
+
+    public static final int FAIL = -1;
+
+    public ExecutionResult() {
+    }
+
+    public static <T> ExecutionResult<T> newInstance() {
+        return new ExecutionResult<T>();
+    }
+
+    public static <T> ExecutionResult<T> newInstance(Class<T> clazz) {
+        return new ExecutionResult<T>();
+    }
+
+    public ExecutionResult<E> success(String message, E data) {
+        this.code = SUCCESS;
+        this.message = message;
+        this.data = data;
+        return this;
+    }
+
+    public ExecutionResult<E> success(String message) {
+        this.code = SUCCESS;
+        this.message = message;
+        return this;
+    }
+
+    public ExecutionResult<E> fail(Integer code, String message, E data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        return this;
+    }
+
+    public ExecutionResult<E> fail(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+        return this;
+    }
+
+    public ExecutionResult<E> fail(String message, E data) {
+        return fail(FAIL, message, data);
+    }
+
+    public ExecutionResult<E> fail(String message) {
+        return fail(FAIL, message);
+    }
+
+    private Integer code;
+    private String message;
+    private E data;
+
+}
